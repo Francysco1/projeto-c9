15689475763c809e76e29589645769c236cbec8d
Francysco1 <94544368+Francysco1@users.noreply.github.com>
2021-11-24 11:35:10 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,34 @@
+var bola;
+
+
+function setup() {
+  createCanvas(400,400);
+  bola=createSprite(200,200,30,30);
+}
+
+
+function draw() 
+{
+  background("yellow");
+
+if (keyIsDown(UP_ARROW)) {
+  bola.position.y=bola.position.y-5;
+  
+}
+if (keyIsDown(DOWN_ARROW)) {
+  bola.position.y=bola.position.y+5;
+}
+if (keyIsDown(RIGHT_ARROW)) {
+  bola.position.x=bola.position.x+5;
+}
+if (keyIsDown(LEFT_ARROW)) {
+  background("red")
+
+  bola.position.x=bola.position.x-5;
+}
+  drawSprites();
+}
+
+
+
+
